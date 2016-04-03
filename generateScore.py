0c3b203c7f5a2f57816b1bf820e0e48854305cf1
Anant Maheshwari <anantm95@gmail.com>
2016-04-04 01:55:14 +0530

    Code addition for plural noun, comparative and superlative adjectives

--- generateScore.py
+++ generateScore.py
@@ -5,7 +5,7 @@ from BoW import *
 from bs4 import BeautifulSoup
 
 
-review1 = "This is a very good product. These speakers have an amazing base but very bad treble. \
+review1 = "This is a very good product. These speakers have an amazing bases but very bad treble. \
 Also, the speakers are made of high-class wood and hence an extraordinary sound. Worth buying. This product is \
 well-made, and has a good durability. I received the product in not so good packaging which was the only \
 issue. The speakers are worth for it's price. Go for it without thinking."
@@ -40,7 +40,12 @@ tagged_tokens = nltk.pos_tag(tokens)
 
 #print tagged_tokens
 
-chunk_grammar = "NP: {<RB>?<JJ>+<NN>}"
+max_positive = max_positive_words()
+min_positive = min_positive_words()
+max_negative = max_negative_words()
+min_negative = min_negative_words()
+
+chunk_grammar = "NP: {<RB|RBR|RBS>?<JJ>+<NN|NNS>}"
 chunk_parser = nltk.RegexpParser(chunk_grammar)
 chunk_tree = chunk_parser.parse(tagged_tokens)
 
@@ -49,15 +54,6 @@ np_list = []
 for subtree in chunk_tree.subtrees():
 	if (subtree.label() == 'NP'):
 		np_list.append(subtree.leaves())
-
-max_positive = max_positive_words()
-min_positive = min_positive_words()
-max_negative = max_negative_words()
-min_negative = min_negative_words()
-
-#print 'hello'
-#print max_positive
-#print min_positive
 
 print np_list;
 print negativeWords['bad']
@@ -69,7 +65,7 @@ for phrase in np_list:
 	for (word, tag) in phrase:
 		#print phrase
 		
-		if(tag == 'RB'):
+		if(tag == 'RB' or tag == 'RBR' or tag == 'RBS'):
 			intensify = 1
 
 		if (tag == 'JJ'):
@@ -95,7 +91,7 @@ for phrase in np_list:
 				if (intensify == 1):
 					feature_score *= 1.2
 
-		if (tag == 'NN'):
+		if (tag == 'NN' or tag == 'NNS'):
 			print word, " ", feature_score
 	
 
@@ -119,16 +115,3 @@ for adj in adjectives:
 avg_total_score = total_score/len(adjectives)
 
 print "\nTotal Score: ", avg_total_score, "\n"
-
-'''feed_xml = urllib2.urlopen('http://feeds.bbci.co.uk/news/rss.xml').read()
-feed = BeautifulSoup(feed_xml.decode('utf8'), "lxml")
-to_summarize = map(lambda p: p.text, feed.find_all('guid'))
-
-fs = FrequencySummarizer()
-for article_url in to_summarize[:5]:
-    title, text = get_only_text(article_url)
-    print '----------------------------------'
-    print title
-    for s in fs.summarize(text, 2):
-    	print '*',s'''
-
